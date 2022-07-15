210148821c1cf39a17ec59bc3e165a5d5adfe38b
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-07-15 15:43:57 +0200

    Add Prettier

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import "./app.css";
-import App from "./App.svelte";
+import './app.css'
+import App from './App.svelte'
 
-let target: Element | null = document.querySelector("#app");
+let target: Element | null = document.querySelector('#app')
 
 if (!target) {
-  target = document.createElement("div");
-  document.body.append(target);
+  target = document.createElement('div')
+  document.body.append(target)
 }
 
-const app = new App({ target });
+const app = new App({ target })
 
-export default app;
+export default app
--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
-import sveltePreprocess from "svelte-preprocess";
+import sveltePreprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/vite-plugin-svelte').SvelteOptions} */
 const svelteConfig = {
   preprocess: sveltePreprocess(),
-};
+}
 
-export default svelteConfig;
+export default svelteConfig
