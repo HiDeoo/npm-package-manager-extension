c906e9f5dc73e9b2125edf2674c8e87bb614508b
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-07-15 19:36:23 +0200

    Add options & popup apps

---
+++ src/options/Options.svelte
@@ -0,0 +1,1 @@
+<main>OPTIONS</main>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
+import { resolve } from 'node:path'
+
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      input: {
+        options: resolve(__dirname, 'options.html'),
+        popup: resolve(__dirname, 'popup.html'),
+      },
+    },
+  },
+  plugins: [tsconfigPaths(), svelte()],
 })
