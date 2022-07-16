a28ef426215daafa524621c6fe55a7d7060b2fe0
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-07-16 17:35:32 +0200

    Bind toggling of default command to options

--- src/libs/storage.ts
+++ src/libs/storage.ts
@@ -1,6 +1,7 @@
 import type { PackageManager } from '@/libs/packageManager'
 
 const defaultOptions: Options = {
+  enabled: true,
   packageManager: 'pnpm',
 }
 
@@ -12,6 +13,24 @@ export function setOptions(options: Partial<Options>) {
   return chrome.storage.sync.set(options)
 }
 
+export function addOptionsListener(listener: (newOptions: Options, optionChanges: OptionsChanges) => void) {
+  return chrome.storage.onChanged.addListener(async (changes) => {
+    const newOptions = await getOptions()
+
+    listener(newOptions, changes as OptionsChanges)
+  })
+}
+
 export interface Options {
+  enabled: boolean
   packageManager: PackageManager
+}
+
+type OptionsChanges = {
+  [TKey in keyof Options]: Change<Options[TKey]>
+}
+
+interface Change<TType> {
+  newValue: TType
+  oldValue: TType
 }
--- src/popup/Popup.svelte
+++ src/popup/Popup.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
-  import Switch from '@/components/Switch.svelte'
+  import { onMount } from 'svelte'
 
-  let checked = false
+  import Switch from '@/components/Switch.svelte'
+  import { getOptions, setOptions, type Options } from '@/libs/storage'
+
+  let options: Options | undefined
+
+  onMount(async () => {
+    options = await getOptions()
+  })
+
+  $: options && setOptions({ enabled: options.enabled })
 </script>
 
 <main>
-  <Switch bind:checked label="Enabled????" />
+  {#if options}
+    <Switch bind:checked={options.enabled} label="Enabled????" />
+  {:else}
+    <p>// TODO</p>
+  {/if}
 </main>
