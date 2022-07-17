262249b9ff652db63acadd0bb9833ee2a4c73e82
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-07-17 19:53:26 +0200

    Add issue reporting link

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,16 +1,15 @@
 <footer>
-  <a href="x">Link 1</a>
-  <a href="x">Link 2</a>
+  <a
+    href="https://github.com/HiDeoo/npm-package-manager-extension/issues"
+    title="Report an issue with the Npm Package Manager extension"
+    target="_blank">Report issue</a
+  >
 </footer>
 
 <style>
   footer {
     display: flex;
-    justify-content: space-between;
+    justify-content: center;
     padding: var(--size-3);
-  }
-
-  a {
-    display: inline-block;
   }
 </style>
