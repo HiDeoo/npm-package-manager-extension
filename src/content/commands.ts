28fb297b01e8da3b9b8f2021a39560af903eade9
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-07-18 14:18:55 +0200

    Refactor commands related code

---
+++ src/content/commands.ts
@@ -0,0 +1,67 @@
+import { cloneElement, showElement } from '@/libs/html'
+import { getPackageManagerCommand, type PackageManager } from '@/libs/packageManager'
+
+const npmManagerCommandClass = 'npm-package-manager-command'
+
+export function updateCommandTitle(commandTitle: ChildNode, packageManager?: PackageManager) {
+  commandTitle.textContent = packageManager ? `Install with ${packageManager}` : 'Install'
+}
+
+export function createCommandNode(command: HTMLElement, packageManager: PackageManager, dev = false) {
+  const newCommand = cloneElement(command)
+
+  showElement(newCommand)
+  newCommand.classList.add(npmManagerCommandClass)
+
+  const commandButtonNode = newCommand.querySelector('span[role=button]')
+  const dependency = commandButtonNode?.textContent?.split(' ')?.at(-1)
+
+  if (commandButtonNode && dependency) {
+    commandButtonNode.textContent = getPackageManagerCommand(packageManager, dependency, dev)
+
+    commandButtonNode.addEventListener('click', handleCommandClick)
+    commandButtonNode.addEventListener('keypress', handleCommandKeyPress)
+  }
+
+  return newCommand
+}
+
+export function removeCommandNodes() {
+  const commandNodes = document.querySelectorAll(`.${npmManagerCommandClass}`)
+
+  for (const commandNode of commandNodes) {
+    const commandButtonNode = commandNode.querySelector('span[role=button]')
+
+    commandButtonNode?.addEventListener('click', handleCommandClick)
+    commandButtonNode?.addEventListener('keypress', handleCommandKeyPress)
+
+    commandNode.remove()
+  }
+}
+
+export function handleCommandClick(event: Event) {
+  if (!event.currentTarget || !(event.currentTarget instanceof HTMLElement) || !event.currentTarget.textContent) {
+    return
+  }
+
+  copyCommandToClipboard(event.currentTarget.textContent)
+}
+
+export function handleCommandKeyPress(event: Event) {
+  if (
+    !(event instanceof KeyboardEvent) ||
+    event.key !== 'Enter' ||
+    !event.currentTarget ||
+    !(event.currentTarget instanceof HTMLElement) ||
+    !event.currentTarget.textContent
+  ) {
+    return
+  }
+
+  copyCommandToClipboard(event.currentTarget.textContent)
+}
+
+function copyCommandToClipboard(text: string) {
+  // TODO(HiDeoo)
+  console.error('🚨 [index.ts:105] text', text)
+}
--- src/content/index.ts
+++ src/content/index.ts
@@ -1,8 +1,7 @@
-import { cloneElement, hideElement, showElement } from '@/libs/html'
+import { createCommandNode, removeCommandNodes, updateCommandTitle } from '@/content/commands'
+import { hideElement, showElement } from '@/libs/html'
 import { addOptionsListener, getOptions, type Options } from '@/libs/options'
-import { getPackageManagerCommand, isValidPackageManager, type PackageManager } from '@/libs/packageManager'
-
-const npmManagerCommandClass = 'npm-package-manager-command'
+import { isValidPackageManager } from '@/libs/packageManager'
 
 function setup(options: Options) {
   addOptionsListener((optionChanges) => {
@@ -59,69 +58,6 @@ function getNpmElements() {
     command,
     commandTitle,
   }
-}
-
-function updateCommandTitle(commandTitle: ChildNode, packageManager?: PackageManager) {
-  commandTitle.textContent = packageManager ? `Install with ${packageManager}` : 'Install'
-}
-
-function createCommandNode(command: HTMLElement, packageManager: PackageManager, dev = false) {
-  const newCommand = cloneElement(command)
-
-  showElement(newCommand)
-  newCommand.classList.add(npmManagerCommandClass)
-
-  const commandButtonNode = newCommand.querySelector('span[role=button]')
-  const dependency = commandButtonNode?.textContent?.split(' ')?.at(-1)
-
-  if (commandButtonNode && dependency) {
-    commandButtonNode.textContent = getPackageManagerCommand(packageManager, dependency, dev)
-
-    commandButtonNode.addEventListener('click', handleCommandClick)
-    commandButtonNode.addEventListener('keypress', handleCommandKeyPress)
-  }
-
-  return newCommand
-}
-
-function removeCommandNodes() {
-  const commandNodes = document.querySelectorAll(`.${npmManagerCommandClass}`)
-
-  for (const commandNode of commandNodes) {
-    const commandButtonNode = commandNode.querySelector('span[role=button]')
-
-    commandButtonNode?.addEventListener('click', handleCommandClick)
-    commandButtonNode?.addEventListener('keypress', handleCommandKeyPress)
-
-    commandNode.remove()
-  }
-}
-
-function handleCommandClick(event: Event) {
-  if (!event.currentTarget || !(event.currentTarget instanceof HTMLElement) || !event.currentTarget.textContent) {
-    return
-  }
-
-  copyToClipboard(event.currentTarget.textContent)
-}
-
-function handleCommandKeyPress(event: Event) {
-  if (
-    !(event instanceof KeyboardEvent) ||
-    event.key !== 'Enter' ||
-    !event.currentTarget ||
-    !(event.currentTarget instanceof HTMLElement) ||
-    !event.currentTarget.textContent
-  ) {
-    return
-  }
-
-  copyToClipboard(event.currentTarget.textContent)
-}
-
-function copyToClipboard(text: string) {
-  // TODO(HiDeoo)
-  console.error('🚨 [index.ts:105] text', text)
 }
 
 getOptions()
