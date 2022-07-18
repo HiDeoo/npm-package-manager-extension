8c0994326007b51cd140715b55e79742c2b68a0f
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-07-18 15:19:12 +0200

    Add command copy icon animation

--- src/content/commands.ts
+++ src/content/commands.ts
@@ -44,7 +44,7 @@ export function handleCommandClick(event: Event) {
     return
   }
 
-  copyCommandToClipboard(event.currentTarget.textContent)
+  copyCommandToClipboard(event.currentTarget)
 }
 
 export function handleCommandKeyPress(event: Event) {
@@ -58,10 +58,34 @@ export function handleCommandKeyPress(event: Event) {
     return
   }
 
-  copyCommandToClipboard(event.currentTarget.textContent)
+  copyCommandToClipboard(event.currentTarget)
 }
 
-function copyCommandToClipboard(text: string) {
+function copyCommandToClipboard(target: HTMLElement) {
   // TODO(HiDeoo)
-  console.error('🚨 [index.ts:105] text', text)
+  console.error('🚨 [index.ts:105] text', target.textContent)
+
+  const svg = target.parentElement?.parentElement?.querySelector('svg[data-icon="copy"]')
+
+  if (!svg) {
+    return
+  }
+
+  const previousIconPath = svg.querySelector('path')
+
+  if (!previousIconPath) {
+    return
+  }
+
+  const checkIconPath = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+  checkIconPath.setAttribute(
+    'd',
+    'M438.6 105.4C451.1 117.9 451.1 138.1 438.6 150.6L182.6 406.6C170.1 419.1 149.9 419.1 137.4 406.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4C21.87 220.9 42.13 220.9 54.63 233.4L159.1 338.7L393.4 105.4C405.9 92.88 426.1 92.88 438.6 105.4H438.6z'
+  )
+
+  previousIconPath.replaceWith(checkIconPath)
+
+  setTimeout(() => {
+    checkIconPath.replaceWith(previousIconPath)
+  }, 500)
 }
